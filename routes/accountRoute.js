4f9d91937d627e9132236f28e7da84dc57a4463b
Matthew Anderson <manderson0182@gmail.com>
2025-07-24 12:03:43 -0600

    Accounts are posting to the db

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,17 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/index");
+const regValidate = require('../utilities/account-validation')
 
 // Route to build an account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build an register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 // Route to register and account
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post("/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
