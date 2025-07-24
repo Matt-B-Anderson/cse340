1cebff8da8c16c0b18215642399335a9e7dd22cd
Matthew Anderson <manderson0182@gmail.com>
2025-07-24 11:22:38 -0600

    Added validation for the forms

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,8 +6,9 @@ const utilities = require("../utilities/index");
 
 // Route to build an account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
-
+// Route to build an register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
-
+// Route to register and account
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const accountRoute = require("./routes/accountRoute");
 const utilities = require("./utilities/index");
 const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser");
 
 /* ***********************
  * View Engine and Template
@@ -45,6 +46,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+// Register middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * Routes
